17b17c6071f94b00cf385673cb8791770ea23395
François Trible <ftrible@gmail.com>
2024-01-06 17:11:18 +0100

    error

--- src/web_project/views.py
+++ src/web_project/views.py
@@ -12,8 +12,7 @@ def about_page(request):
     return render(request,template, context)
 
 from django.shortcuts import render, redirect                                
-from django.contrib.auth import authenticate, login                          
-from .forms import LoginForm                                                 
+from django.contrib.auth import authenticate, login                                                                          
                                                                                 
 def login_view(request):                                                     
     if request.method == 'POST':                                    
